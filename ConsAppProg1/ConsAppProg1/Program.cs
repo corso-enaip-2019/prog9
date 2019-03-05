60a95dccad33e95fc091a7d8e5ca57e4a994305b
oni93 <oni93@hotmail.it>
2019-03-05 12:48:52 +0100

    eserc.

---
+++ ConsAppProg1/ConsAppProg1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppProg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("riga");
+            //Console.ReadLine();
+
+            //string str = "Hola";
+            //str = string.Concat(str, "viva");
+
+
+            //int i = 0;
+            //if (i > 1 && i < 10)
+            //{
+
+            //}
+            //else if (i > 2 || i == 5)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
+
+            //inserire all'utente 3 numeri interi e diamo come indicazione se quei numeri sono un potenziale triangolo
+
+            int[] lato = new int[3];
+
+            
+            for (int i = 0; i < lato.Length; i++)
+            {
+                lato[i] = AskandCheckNumber("inserisci lato");
+
+            }
+
+
+            if (IsTriangle(lato[0], lato[1], lato[2]))
+            {
+                Console.WriteLine("è un triangolo");
+            }
+            else
+            {
+                Console.WriteLine("non è un triangolo");
+
+
+
+
+
+                do
+                {
+                    int m = Math.Max(Math.Max(lato[0], lato[1]), lato[2]);
+
+                    int index = Array.IndexOf(lato, m);
+
+                    lato[index] = lato[index] -1;
+
+                } while (!IsTriangle(lato[0], lato[1], lato[2]));
+
+                Console.WriteLine($"Questi valori costituiscono un triangolo {lato[0]},{lato[1]},{lato[2]}");
+
+
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Mostra all'utente il messaggio e converte il valore inserito in input in un intero
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+
+        static int AskandCheckNumber(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Concat("Message: ", message));
+            int convertedValue;
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            bool ConversionOk = int.TryParse(input, out convertedValue);
+            if (!ConversionOk)
+            {
+                Console.WriteLine("l'input non è valido");
+            }
+            if (convertedValue <= 0)
+            {
+                Console.WriteLine("il valore deve essere positivo");
+
+            }
+
+            System.Diagnostics.Debug.WriteLine(string.Format("{0}:{1}", message, input));
+            System.Diagnostics.Debug.WriteLine($"{message}:{input}");
+
+            return convertedValue;
+        }
+
+        static bool IsTriangle(int a, int b, int c)
+
+       {
+
+
+            bool sumOk = a < b + c && b < a + c && c < a + b;
+            bool diffOk = a > Math.Abs(b - c) && b > Math.Abs(a - c) && c > Math.Abs(a - b);
+
+            return sumOk && diffOk;
+        }
+    }
+}
---
+++ ConsAppProg1/ConsAppProg1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsAppProg1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsAppProg1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6cb28f0b-d8a3-4413-9609-ba6bd8b30baa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
