dba48e767583f7ff1756d3deda4f91df265900aa
oni93 <oni93@hotmail.it>
2019-03-07 09:14:34 +0100

    aggiornamento del 07/03

---
+++ ConsAppScuola/ConsAppScuola/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppScuola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input : nome e classe(5 classi)
+            //output: classe : nome degli studenti
+
+            const string Si = "s";
+
+            do
+            {
+                Menu();
+
+
+                Console.WriteLine("Vuoi tornare al menu principale? (s/n)");
+            } while (Console.ReadLine().Equals(Si, StringComparison.InvariantCultureIgnoreCase));
+
+
+
+            Console.ReadLine();
+        }
+
+        static Dictionary<int, List<string>> _elementiClasse = new Dictionary<int, List<string>>();
+
+
+        static void Menu()
+        {
+            const string Nuovo = "N";
+            const string Lista = "L";
+            const string Fine = "F";
+
+            string sceltaIns;
+
+
+
+            //do
+            //{
+            Console.WriteLine(@"Inserisci una delle opzioni disponibili:
+            'N' = Nuovo studente
+            'L' = Visualizza Lista degli studenti
+            //'C' = cerca uno specifico studente
+            'F' = Fine");
+
+            sceltaIns = Console.ReadLine();
+
+            if (Nuovo.Equals(sceltaIns, StringComparison.InvariantCultureIgnoreCase))
+            {
+                inserimentoStudente(sceltaIns);
+
+
+            }
+            else if (Lista.Equals(sceltaIns, StringComparison.InvariantCultureIgnoreCase))
+            {
+                listaStud(sceltaIns);
+            }
+            else if (Fine.Equals(sceltaIns, StringComparison.InvariantCultureIgnoreCase))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("scelta non valida");
+                Console.ReadLine();
+                return;
+            }
+
+            //    Console.ReadLine();
+            //} while (!Fine.Equals(sceltaIns, StringComparison.InvariantCultureIgnoreCase));
+
+        }
+
+
+
+        static void inserimentoStudente(string a)
+        {
+            int nStudenti;
+            string name;
+
+            Console.WriteLine("Quanti studenti vuoi inserire?");
+            Int32.TryParse(Console.ReadLine(), out nStudenti);
+
+
+            for (int i = 0; i < nStudenti; i++)
+            {
+                int _class;
+
+
+                Console.WriteLine("inserisci il nome dello studente");
+                name = Console.ReadLine();
+
+
+
+                //if (!string.IsNullOrWhiteSpace(name))
+                //{
+
+
+
+                Console.WriteLine("inserisci la classe dello studente");
+                Int32.TryParse(Console.ReadLine(), out _class);
+
+               
+                while ( !(_class > 0 && _class < 6))
+                {
+                    Console.WriteLine("Classe non valida. La classe deve essere compresa tra 1 e 5");
+                    Console.WriteLine("Inserisci nuovamente la classe");
+                    _class=Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (!_elementiClasse.ContainsKey(_class))
+                {
+                    //List<string> studenti = new List<string>();
+                    //studenti.Add(name);
+                    //_elementiClasse.Add(_class, studenti);
+                    _elementiClasse.Add(_class, new List<string>());
+                }
+                //else
+                //{
+
+                //List<string> studenti = _elementiClasse[_class];
+                //studenti.Add(name);
+                //}
+                _elementiClasse[_class].Add(name);
+            }
+        }
+
+        static void listaStud(string a)
+        {
+
+
+            Console.WriteLine("inserisci la classe di cui desideri visualizzare la lista degli studenti");
+            int classeInserita = Convert.ToInt32(Console.ReadLine());
+
+
+            foreach (KeyValuePair<int, List<string>> item in _elementiClasse)
+            {
+                if (item.Key == classeInserita)
+                {
+                    foreach (string ite in item.Value)
+                    {
+                        Console.WriteLine(ite);
+
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+}
+
+
+
+
---
+++ ConsoleAppB/ConsoleAppB/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok = CheckBrackets(@"
+                namespace Brackets
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            bool ok = CheckBrackets("")
+                            Console.ReadLine();
+                        }
+                        static bool CheckBrackets(string text)
+                        {
+                        }
+                    }
+                }");
+
+            string strOk = ok ? "OK" : "KO";
+            Console.WriteLine($"Text is { strOk }");
+
+            Console.ReadLine();
+        }
+
+        static List<char> openingBrackets = new List<char>() { '(', '[', '{' };
+        static List<char> closingBrackets = new List<char>() { ')', ']', '}' };
+        static bool CheckBrackets(string text)
+        {
+            //Spostato in inizializzatore
+            //openingBrackets.Add('(');
+            //openingBrackets.Add('[');
+            //openingBrackets.Add('{');
+
+            //Spostato in inizializzatore
+            //closingBrackets.Add(')');
+            //closingBrackets.Add(']');
+            //closingBrackets.Add('}');
+
+            Stack<char> brackets = new Stack<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                //if (currentChar == '(' || currentChar == '[' || currentChar == '{')
+                if (openingBrackets.Contains(currentChar))
+                {
+                    brackets.Push(currentChar);
+                }
+                //else if (currentChar == ')' || currentChar == ']' || currentChar == '}')
+                else if (closingBrackets.Contains(currentChar))
+                {
+                    //spostato in metodo separato
+                    //char correspondingOpeningBracket = ' ';
+                    //if (currentChar == ')')
+                    //    correspondingOpeningBracket = '(';
+                    //else if (currentChar == ']')
+                    //    correspondingOpeningBracket = '[';
+                    //else if (currentChar == '}')
+                    //    correspondingOpeningBracket = '{';
+
+                    if (brackets.Count == 0)
+                        return false;
+
+                    if (brackets.Peek() == CorrespondingOpeningBracket(currentChar))
+                        brackets.Pop();
+                    else
+                        return false;
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+
+        static char CorrespondingOpeningBracket(char closingBracket)
+        {
+            int closingIndex = closingBrackets.IndexOf(closingBracket);
+            return openingBrackets[closingIndex];
+
+            char correspondingOpeningBracket = ' ';
+            if (closingBracket == ')')
+                correspondingOpeningBracket = '(';
+            else if (closingBracket == ']')
+                correspondingOpeningBracket = '[';
+            else if (closingBracket == '}')
+                correspondingOpeningBracket = '{';
+
+            return correspondingOpeningBracket;
+        }
+
+        #region Alternativa
+
+        static Dictionary<char, char> _brackets = new Dictionary<char, char> {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
+
+        /// <summary>
+        /// Verifica se il testo passato contiene un numero coerente di parentesi
+        /// di apertura e chiusura "(", "[", "{"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static bool CheckBrackets2(string text)
+        {
+            Stack<char> brackets = new Stack<char>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (_brackets.ContainsKey(currentChar))
+                {
+                    brackets.Push(currentChar);
+                }
+                else if (_brackets.ContainsValue(currentChar))
+                {
+                    if (brackets.Count == 0)
+                        return false;
+
+                    if (brackets.Peek() == MatchingBracket(currentChar))
+                        brackets.Pop();
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+
+        private static char MatchingBracket(char currentChar)
+        {
+            foreach (var item in _brackets)
+            {
+                if (item.Value == currentChar)
+                    return item.Key;
+            }
+
+            return ' ';
+        }
+
+        #endregion
+    }
+}
