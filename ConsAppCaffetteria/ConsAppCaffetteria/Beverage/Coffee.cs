0a1556e0af5109693609d7e31bba0a22e3f4d400
oni93 _ <oni93@hotmail.it>
2019-03-22 08:36:27 +0100

    caffetteria con interfaccia

---
+++ ConsAppCaffetteria/ConsAppCaffetteria/Beverage/Coffee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppCaffetteria.Beverage
+{
+    public class Coffee : ICoffee
+    {
+
+        readonly double _cost;
+        readonly string _name;
+
+        public Coffee(double cost,string name)
+        {
+            _cost = cost;
+            _name = name;
+        }
+
+
+        public string Description { get {return $"{ _cost }{_name}" ; } } 
+
+        public double Cost { get { return _cost; } }
+    }
+}
---
+++ ConsAppCaffetteria/ConsAppCaffetteria/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsAppCaffetteria.Beverage;
+
+namespace ConsAppCaffetteria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ICoffee> coffees = new List<ICoffee>();
+            coffees.Add(new Coffee(1.00, "Caffè"));
+            coffees.Add(new Coffee(1.10, "Mocaccino"));
+
+            foreach(ICoffee coffee in coffees)
+            {
+
+                
+                Console.WriteLine(coffees);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
