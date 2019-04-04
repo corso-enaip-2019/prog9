dc62d50d855e9808b04f9d298586263e63c2336e
oni93 _ <oni93@hotmail.it>
2019-04-04 19:05:47 +0200

    Esercizio Smarphone corretto

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -10,36 +10,23 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            List<Smartphone> list = SmartphoneList();
+            List<Smartphone> list = CreateSmartphoneList();
 
-            IEnumerable<Smartphone> color = Filter<Smartphone>(list,new ColorFilter());
+            IEnumerable<Smartphone> blackFilteredSmartphone = Filter<Smartphone>(list, new BlackColorFilter());
 
-            foreach (Smartphone item in color)
+            foreach (Smartphone item in blackFilteredSmartphone)
             {
-                Console.WriteLine($"{item._model}{item._version}{item._cost}{item._color}");
+                Console.WriteLine($"{item.Model} {item.Version} {item.Cost} {item.Color}");
             }
-
+            
 
             Console.ReadLine();
 
         }
 
-        class Smartphone
-        {
-            public string _model;
-            public string _version;
-            public decimal _cost;
-            public string _color;
 
-            public Smartphone(string Model,string Version, decimal Cost,string Color)
-            {
-                Model = _model;
-                Version = _version;
-                Cost = _cost;
-                Color = _color;
-            }
-        }
-        private static List<Smartphone> SmartphoneList()
+
+        static List<Smartphone> CreateSmartphoneList()
         {
             return new List<Smartphone>
             {
@@ -52,35 +39,9 @@ namespace Linq
             };
         }
 
-        interface IFilter<T>
+        static IEnumerable<T> Filter<T>(IEnumerable<T> input, IFilter<T> condition)
         {
-
-            bool Filter(T item);
-
-        }
-
-        interface IProject<TInput, TOutput>
-        {
-            TOutput Project(TInput item);
-        }
-
-        class ColorFilter: IFilter<Smartphone>
-        {
-            public bool Filter(Smartphone s)
-            {
-                
-
-                return s._color == "Black";
-
-
-
-            }
-
-        }
-
-        private static IEnumerable<T> Filter<T>(IEnumerable<T> input, IFilter<T> condition)
-        {
-            List <T> output = new List<T>();
+            List<T> output = new List<T>();
 
             foreach (T item in input)
             {
@@ -94,10 +55,7 @@ namespace Linq
             return output;
         }
 
-
-
-
-        //        Creare una classe per smartphone, con proprietà: modello, versione, costo, colore.
+        //Creare una classe per smartphone, con proprietà: modello, versione, costo, colore.
 
         //Crea una lista con gli smartphone finti.
 
@@ -105,4 +63,45 @@ namespace Linq
 
         //Crea un metodo che filtra la lista restituendo solo quelli che costano meno di una certa quantità(passati come parametro).
     }
+    
+    interface IFilter<T>
+    {
+
+        bool Filter(T item);
+
+    }
+
+    interface IProject<TInput, TOutput>
+    {
+        TOutput Project(TInput item);
+    }
+
+    class Smartphone
+    {
+        public string Model { get; }
+        public string Version { get; }
+        public decimal Cost { get; }
+        public string Color { get; }
+
+        public Smartphone(string model, string version, decimal cost, string color)
+        {
+            Model = model;
+            Version = version;
+            Cost = cost;
+            Color = color;
+        }
+    }
+
+    class BlackColorFilter : IFilter<Smartphone>
+    {
+        public bool Filter(Smartphone s)
+        {
+            
+            return s.Color == "Black";
+            
+        }
+
+    }
+
+    
 }
