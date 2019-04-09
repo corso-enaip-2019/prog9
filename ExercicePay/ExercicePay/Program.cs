cd5ec1889f7035ea8a992644ecda9755736f2e3b
oni93 _ <oni93@hotmail.it>
2019-04-10 00:08:51 +0200

    Non funzionante...

--- ExercicePay/ExercicePay/Program.cs
+++ ExercicePay/ExercicePay/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,26 @@ namespace ExercicePay
         static void Main(string[] args)
         {
 
+            DateTime dt = new DateTime(2019, 01, 31);
+
+            List<Employee> employees = employeesList();
+            foreach (var item in employees)
+            {
+                if (item.IsPayDay(dt))
+                {
+                    Console.WriteLine($"{item.Id} {item.Name}");
+                    item.CalcolatePay();
+                    Console.WriteLine($"{dt}");
+                }
+            }
+
+
+
+
+
+
+            Console.Read();
+
             /*
             lista di impiegati creata come mock
             per ogni impiegato della lista
@@ -21,49 +42,127 @@ namespace ExercicePay
              */
         }
 
-        class Employee
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public decimal TotalPayment { get; set; }
-            public decimal Pay = 0;
-            public bool IsPayDay;
+        
 
-            public virtual decimal CalcolatePay()
+        public static List<Employee> employeesList()
+        {
+            return new List<Employee>
             {
-                return Pay;
-            }
-            
+                new FixedSalary{Id = 01, Name = "Stefano Aspire"},
+                new FixedSalary{Id = 03, Name = "Joseph Joestar"},
+                new HourlyPay{Id = 02,Name = "Giacomo Leopardi"},
+                new HourlyPay{Id = 07,Name = "Jean Claude Van Damme"},
+                new HourlyPay{Id = 05, Name ="Sasha Grey"},
+                new CommissionPay{Id = 04, Name ="Tom Cruise"},
+            };
+        }
+    }
+
+    abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TotalPayment { get; set; }
+        public abstract bool IsPayDay(DateTime date);
+        public abstract void CalcolatePay();
+    }
+
+    class FixedSalary : Employee
+    {
+        public decimal FPay = 1500;
+
+        public override bool IsPayDay(DateTime date)
+        {
+            if (date == GetLastDayOfMonth())
+                return true;
+            else
+                return false;
         }
 
-        class FixedSalary: Employee
+        public override void CalcolatePay()
         {
-            public decimal Pay = 1500;
-            public override decimal CalcolatePay()
+            Console.WriteLine( FPay ); 
+        }
+
+        public static DateTime GetLastDayOfMonth()
+        {
+            DateTime dt = new DateTime();
+            dt = dt.AddMonths(1);
+            dt = dt.AddDays(-dt.Day);
+
+            return dt;
+        }
+    }
+
+    class HourlyPay : Employee
+    {
+        public decimal HPay = 0;
+        public decimal oreLavorate { get; set; }
+
+        public override bool IsPayDay(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+                return true;
+            else
+                return false;
+        }
+
+        public override void CalcolatePay()
+        {
+            HPay = oreLavorate * 10;
+
+            Console.WriteLine( HPay ); 
+        }
+    }
+
+    class CommissionPay : Employee
+    {
+        public decimal CommPay = 0;
+      
+        public override bool IsPayDay(DateTime date)
+        {
+            return true;
+        }
+
+        public static List<Record> records()
+        {
+            return new List<Record>
             {
-                return Pay;
-            }
-
+                new Record{Vendita=353.6m ,  Date = new DateTime(2019,1,10)},
+                new Record{Vendita=1300m ,  Date = new DateTime(2019,1,11)},
+                new Record{Vendita=1560.30m ,  Date = new DateTime(2019,1,11)},
+                new Record{Vendita=1200m ,  Date = new DateTime(2019,1,15)},
+                new Record{Vendita=750.5m ,  Date = new DateTime(2019,1,31)},
+                new Record{Vendita=800.7m ,  Date = new DateTime(2019,1,31)},
+                new Record{Vendita=400m ,  Date = new DateTime(2019,1,20)},
+            };
         }
 
-        class HourlyPay:Employee
+        public override void CalcolatePay()
         {
-            public decimal Pay = 0;
-            public override decimal CalcolatePay()
-            {
-
-
-                return base.CalcolatePay();
-            }
         }
 
-        class CommissionPay:Employee
-        {
+        //public override void CalcolatePay(List<Record> list, DateTime date)
+        //{
 
+        //    decimal DailyPay = 0;
+        //    foreach (var item in list)
+        //    {
+        //        if (date == item.Date)
+        //        {
+        //            DailyPay += (item.Vendita / 100) * 10;
+        //        }
 
-        }
+        //    }
 
+        //    Console.WriteLine(DailyPay); 
+        //}
+    }
 
+    class Record
+    {
+        public decimal Vendita { get; set; }
+        public DateTime Date { get; set; }
     }
 }
 /*
