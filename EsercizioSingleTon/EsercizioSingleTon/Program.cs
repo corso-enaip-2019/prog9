548b646bb05e830c2841780db97c36a61d7f69da
oni93 <oni93@hotmail.it>
2019-04-12 11:56:43 +0200

    Exercice Singleton

---
+++ EsercizioSingleTon/EsercizioSingleTon/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioSingleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person Mario = new Person("Mario", 35, 1500);
+
+            JsonSaver.Instance.SaveOnFile(Mario);
+            XmlSaver.Instance.SaveOnFile(Mario);
+           
+
+            Console.Read();
+
+        }
+    }
+    
+
+
+    class Person
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public Person(string fullname,int age,decimal salary)
+        {
+            FullName = fullname;
+            Age = age;
+            Salary = salary;
+
+
+        }
+
+    }
+
+    abstract class BaseSaver
+    {
+        public void SaveOnFile(Person p)
+        {
+            string formatted = FormatAsString(p);
+
+            SaveOnFile(formatted);
+        }
+
+        protected abstract string FormatAsString(Person p);
+
+        private void SaveOnFile(string formatted)
+        {
+            var fileName = GetFileName();
+
+            var path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                fileName);
+
+            File.WriteAllText(path, formatted);
+        }
+
+        protected abstract string GetFileName();
+    }
+
+    class JsonSaver : BaseSaver
+    {
+
+        static JsonSaver()
+        {
+            Instance = new JsonSaver();
+        }
+
+        public static JsonSaver Instance {get;}
+
+        private JsonSaver() { }
+
+        protected override string FormatAsString(Person p)
+        {
+            return
+                "{" +
+                    $"\"{nameof(Person.FullName)}\":\"{p.FullName}\"," +
+                    $"\"{nameof(Person.Age)}\":{p.Age}," +
+                    $"\"{nameof(Person.Salary)}\":{p.Salary}" +
+                "}";
+        }
+
+        protected override string GetFileName()
+        {
+            return "person-as-json.txt";
+        }
+    }
+
+    class XmlSaver : BaseSaver
+    {
+        static XmlSaver()
+        {
+
+            Instance = new XmlSaver();
+        }
+
+        public static XmlSaver Instance { get; }
+
+        private XmlSaver() { }
+
+        protected override string FormatAsString(Person p)
+        {
+            return
+                $"<{nameof(Person)}>" +
+                    $"<{nameof(Person.FullName)}>{p.FullName}</{nameof(Person.FullName)}>" +
+                    $"<{nameof(Person.Age)}>{p.Age}</{nameof(Person.Age)}>" +
+                    $"<{nameof(Person.Salary)}>{p.Salary}</{nameof(Person.Salary)}>" +
+                $"</{nameof(Person)}>";
+        }
+
+        protected override string GetFileName()
+        {
+            return "person-as-xml.txt";
+        }
+    }
+
+
+    
+}
---
+++ EsercizioSingleTon/EsercizioSingleTon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EsercizioSingleTon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EsercizioSingleTon")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29948745-ebcc-4777-87cf-28a7c39c32ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
