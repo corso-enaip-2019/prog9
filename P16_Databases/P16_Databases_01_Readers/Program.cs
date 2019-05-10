e6a120ce5bf93d1905f97d778049f823a2d5e2f9
oni93 <oni93@hotmail.it>
2019-05-10 09:00:15 +0200

    Aggiunta Readers1 e Readers2

---
+++ P16_Databases/P16_Databases_01_Readers/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P16_Databases_01_Readers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddBonus(0.7, 3, 100);
+
+            Console.Write("Operation successful, press a key to exit...");
+            Console.Read();
+        }
+
+        static void AddBonus(double minProductivity, int maxCount, int bonus)
+        {
+            var employees = ReadEmployeesFromDb();
+
+            var employeesAboveProd = employees
+                .Where(x => x.Productivity > minProductivity)
+                .ToList();
+
+            if (employeesAboveProd.Count > maxCount)
+            {
+                employeesAboveProd = employeesAboveProd
+                    .OrderByDescending(x => x.Productivity)
+                    .Take(maxCount)
+                    .ToList();
+            }
+
+            foreach (var e in employeesAboveProd)
+                e.TotalBonus += bonus;
+            //equivalente: employeesAboveProd.ForEach(e => e.TotalBonus += bonus);
+
+            UpdateEmployees(employeesAboveProd);
+        }
+
+        private static List<Employee> ReadEmployeesFromDb()
+        {
+            var result = new List<Employee>();
+
+            using (var conn = new SqlConnection(@"Server=TRISRV10\SQLEXPRESS;Database=CS2019_Kraus_1;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT Id, Name, Productivity, TotalBonus FROM Employees";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var e = new Employee
+                            {
+                                Id = (int)reader[0],                // indexer per indice di colonna
+                                Name = (string)reader["Name"],      // indexer per nome di colonna
+                                Productivity = reader.GetDouble(2), // metodo tipizzato per indice di colonna
+                                TotalBonus = reader.GetInt32(3),    // i metodi tipizzati sono diversi: per int, double, string, DateTime, ...
+                            };
+                            result.Add(e);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void UpdateEmployees(List<Employee> employees)
+        {
+            using (var conn = new SqlConnection(@"Server=TRISRV10\SQLEXPRESS;Database=CS2019_Kraus_1;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                var updates = employees
+                    .Select(e => CreateCmdForUpdate(e, conn));
+
+                foreach (var cmd in updates)
+                    cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static SqlCommand CreateCmdForUpdate(Employee e, SqlConnection conn)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"UPDATE Employees SET TotalBonus = {e.TotalBonus} WHERE Id = {e.Id}";
+
+            return cmd;
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Productivity { get; set; }
+        public int TotalBonus { get; set; }
+    }
+}
---
+++ P16_Databases/P16_Databases_02_Readers/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P16_Databases_02_Readers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol1 = P16_Databases_02_Readers_Solution1.Application.ReadFriends();
+
+            var sol2 = P16_Databases_02_Readers_Solution2.Application.ReadFriends();
+
+            Console.Write("Operation successful, press a key to exit...");
+            Console.Read();
+        }
+    }
+}
+
+namespace P16_Databases_02_Readers_Solution1
+{
+    public static class Application
+    {
+        public static List<Friend> ReadFriends()
+        {
+            var result = new List<Friend>();
+
+            using (var conn = new SqlConnection(@"Server=TRISRV10\SQLEXPRESS;Database=CS2019_Kraus_1;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText =
+                        " SELECT f.Id, f.Name, f.BestFriendId, ff.Name as BestFriendName" +
+                        " FROM Friends f JOIN Friends as ff on f.BestFriendId = ff.Id";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var e = new Friend
+                            {
+                                Id = (int)reader["Id"],
+                                Name = (string)reader["Name"],
+                                BestFriend = reader["BestFriendId"] != null
+                                ? new Friend
+                                    {
+                                        Id = (int)reader["BestFriendId"],
+                                        Name = (string)reader["BestFriendName"],
+                                    }
+                                : null
+                            };
+                            result.Add(e);
+                        }
+                    }
+                }
+            }
+
+            foreach (var f in result)
+                f.BestFriend = result.FirstOrDefault(x => x.Id == f.BestFriend.Id);
+
+            return result;
+        }
+    }
+
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Friend BestFriend { get; set; }
+    }
+}
+
+namespace P16_Databases_02_Readers_Solution2
+{
+    public static class Application
+    {
+        public static List<Friend> ReadFriends()
+        {
+            var entities = new List<FriendEntity>();
+
+            using (var conn = new SqlConnection(@"Server=TRISRV10\SQLEXPRESS;Database=CS2019_Kraus_1;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText =
+                        " SELECT Id, Name, BestFriendId FROM Friends";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var e = new FriendEntity
+                            {
+                                Id = (int)reader["Id"],
+                                Name = (string)reader["Name"],
+                                BestFriendId = reader["BestFriendId"] == DBNull.Value
+                                    ? null
+                                    : (int?)reader["BestFriendId"]
+                            };
+                            entities.Add(e);
+                        }
+                    }
+                }
+            }
+
+            var result = entities
+                .Select(e => new Friend
+                    {
+                        Id = e.Id,
+                        Name = e.Name
+                    })
+                .ToList();
+
+            foreach (var f in result)
+            {
+                var entity = entities.First(e => e.Id == f.Id);
+
+                if (entity.BestFriendId != null)
+                    f.BestFriend = result.First(x => x.Id == entity.BestFriendId);
+            }
+
+            return result;
+        }
+    }
+
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Friend BestFriend { get; set; }
+    }
+
+    public class FriendEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? BestFriendId { get; set; }
+    }
+}
+
+namespace P16_Databases_02_Readers_Solution3
+{
+    public static class Application
+    {
+        public static List<Friend> ReadFriends()
+        {
+            var result = new List<Friend>();
+
+            using (var conn = new SqlConnection(@"Server=TRISRV10\SQLEXPRESS;Database=CS2019_Kraus_1;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText =
+                        " SELECT Id, Name, BestFriendId FROM Friends";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var e = new Friend
+                            {
+                                Id = (int)reader["Id"],
+                                Name = (string)reader["Name"],
+                                BestFriendId = reader["BestFriendId"] == DBNull.Value
+                                    ? 0
+                                    : (int)reader["BestFriendId"]
+                            };
+                            result.Add(e);
+                        }
+                    }
+                }
+            }
+
+            foreach (var f in result)
+                if (f.BestFriendId != 0)
+                    f.BestFriend = result.First(x => x.Id == f.BestFriendId);
+
+            return result;
+        }
+    }
+
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Friend BestFriend { get; set; }
+        public int BestFriendId { get; set; }
+    }
+}
