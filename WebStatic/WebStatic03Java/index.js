d445716d063b0371755ae51e61195c5ed0aea84b
oni93 <oni93@hotmail.it>
2019-05-24 17:50:47 +0200

    Esercizio Tris

--- WebStatic/WebStatic03Java/index.js
+++ WebStatic/WebStatic03Java/index.js
@@ -1,7 +1,30 @@
-var mario = {
-    name: 'Mario',
-    surname: 'Rossi',
-    birth: new Date("1970-05-04"),
-    gender: 'M',
-    isMarried: true,
-};
+function Stack () {
+
+    this._items = [];
+
+}
+// Stack.prototype.Push = function(item){
+//     this._items.Push(item)
+// }
+
+
+// Stack.prototype.pop = function(){
+//     if(!this._items.length){
+//         throw new Error('Cannot pop from empty stack!');
+        
+//     }
+//     return this._items.pop();
+// }
+
+
+// var elenco = new Stack()
+// elenco.Push(8)
+// console.log(elenco)
+// elenco.Push(9)
+// console.log(elenco)
+var elenco = new Stack();
+elenco._items.push(5)
+
+console.log(elenco)
+
+
