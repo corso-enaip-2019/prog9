8084e7a4cf9fca5e081c8adfbacfcb2572616db4
oni93 <oni93@hotmail.it>
2019-05-28 14:52:11 +0200

    Tria

---
+++ WebStatic/WebStatic03Java/lamba.js
@@ -0,0 +1,16 @@
+var moltiplicazione = function(x,y){
+    return x*y;
+};
+
+var prodotto = moltiplicazione(5,3);
+
+console.log(prodotto)
+
+var divisione = (x,y) => x/y;
+
+var quoziente = divisione(10,5)
+
+console.log(quoziente)
+
+
+
